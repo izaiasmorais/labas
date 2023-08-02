63528c84c3bcdd401ad41ccc6270dc17cc76af78
Izaias <izaiaslima356@gmail.com>
2023-08-02 13:06:22 -0300

    atualizado para orientação a objetos

--- index.py
+++ index.py
@@ -1,31 +1,30 @@
 import requests
-from io import BytesIO
 from PIL import Image
 
 
 class Manipulador:
+
     def __init__(self, url):
         self.url = url
 
     def baixar_imagem(self):
-        resposta = requests.get(self.url)
-        if resposta.status_code == 200:
-            return resposta.content
-        else:
-            print(
-                f'Ocorreu um erro ao tentar baixar a imagem da URL: {self.url}')
-            return None
-
-    def salvar_imagem(self):
-        dados_da_imagem = self.baixar_imagem()
-        if (dados_da_imagem):
-            with open("image.jpg", 'wb') as f:
-                f.write(dados_da_imagem)
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            with open("imagem.jpg", 'wb') as file:
+                file.write(response.content)
                 print('Imagem salva com sucesso em URL: imagem.jpg')
-        else:
-            print('Não foi possível salvar a imagem')
+        except requests.exceptions.MissingSchema:
+            print("URL inválida.")
+        except requests.exceptions.RequestException as e:
+            print(f"Erro na conexão: {e}")
+
+    def mostrar_imagem(self):
+        Image.open("imagem.jpg").show()
 
 
-teste = Manipulador("https://i.imgur.com/4JfFzfh.png")
+manipulador = Manipulador("https://i.imgur.com/4JfFzfh.png")
 
-teste.salvar_imagem()
+manipulador.baixar_imagem()
+
+manipulador.mostrar_imagem()
