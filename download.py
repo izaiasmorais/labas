3f58f80a6e63d930f00527d62375bfd15b313118
HevertonMac <hevertonmacedo23@gmail.com>
2023-08-05 12:23:22 -0300

    adicionado atributo caminho a classe Download e criada a classa Imagem

--- download.py
+++ download.py
@@ -1,19 +1,19 @@
 import requests
 from PIL import Image
 
-
 class Download:
 
-    def __init__(self, url):
+    def __init__(self, url, caminho):
         self.url = url
+        self.caminho_do_arquivo = caminho
 
     def baixar_imagem(self):
         try:
             response = requests.get(self.url)
             response.raise_for_status()
-            with open("imagem.jpg", 'wb') as file:
+            with open(caminho_do_arquivo, 'wb') as file:
                 file.write(response.content)
-                print('Imagem salva com sucesso em URL: imagem.jpg')
+                print(f'Imagem salva com sucesso em URL: {self.caminho_do_arquivo}')
         except requests.exceptions.MissingSchema:
             print("URL inválida.")
         except requests.exceptions.RequestException as e:
---
+++ imagem.py
@@ -0,0 +1,16 @@
+from PIL import Image
+class Imagem:
+    def __init__(self, id, nome, caminho):
+        self.id = id
+        self.nome = nome
+        self.caminho = caminho
+        self.imagem = None
+        try:
+            image.open(self.caminho)
+        except Exception as e:
+            print("Erro ao criar a imagem")
+
+    def mostrar_imagem(self):
+        Image.open(self.camimho).show()
+
+    
