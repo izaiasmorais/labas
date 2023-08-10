6c6314f09bc82e58f7ecca619c19032e5d678ade
gutoportelaa <128807987+gutoportelaa@users.noreply.github.com>
2023-08-10 17:28:40 +0000

    Modelo de alrteração

--- filtros.py
+++ filtros.py
@@ -1,3 +1,23 @@
+from PIL import Image
+import requests
+'''
+@@@@@@@@@@@@@@@@@@
+#Função coletora da Imagem
+@@@@@@@@@@@@@@@@@@
+'''
+def imagem_to_filtrate
+
+
+
+
+
+
+'''
+@@@@@@@@@@@@@@@@@@
+# Sequencia de classes de filtros 
+@@@@@@@@@@@@@@@@@@
+'''
+
 # [] EdgesFilter
 # [] GrayscaleFilter
 # [] BlackAndWhiteFilter
