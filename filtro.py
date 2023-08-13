ef9ea3ca1030bff6885b4147295c3ba86add6c3d
HevertonMac <hevertonmacedo23@gmail.com>
2023-08-13 15:36:51 -0300

    modelo entidades filtros

---
+++ filtro.py
@@ -0,0 +1,2 @@
+#Os filtros disponíveis são: Escala de Cinza, Preto e Branco, Filtro Cartoon, Modo Foto
+#Negativa (inversão das cores da imagem), Modo Contorno e Modo Blurred.
--- filtros.py
+++ filtros.py
@@ -1,23 +1,15 @@
-from PIL import Image
-import requests
-'''
-@@@@@@@@@@@@@@@@@@
-#Função coletora da Imagem
-@@@@@@@@@@@@@@@@@@
-'''
-def imagem_to_filtrate
+from PIL import ImageFilter
 
+#Os filtros disponíveis são: Escala de Cinza, Preto e Branco, Filtro Cartoon, Modo Foto
+#Negativa (inversão das cores da imagem), Modo Contorno e Modo Blurred.
 
+class Filtro_preto_branco:
+    def aplicar_filtro(self, image):
+        grayscale_image = image.convert("L")
+        black_white_image = grayscale_image.convert("1")
+        return black_white_image
 
-
-
-
-'''
-@@@@@@@@@@@@@@@@@@
-# Sequencia de classes de filtros 
-@@@@@@@@@@@@@@@@@@
-'''
-
-# [] EdgesFilter
-# [] GrayscaleFilter
-# [] BlackAndWhiteFilter
+class Filtro_escala_cinza:
+    def aplicar_filtro(self, image):
+        grayscale_image = image.convert("L")
+        return grayscale_image
