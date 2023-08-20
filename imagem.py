be2a18b0b44722ebd6208290408f452f549f8b51
Lucas Mendes <56414327+GeyldsonLucas@users.noreply.github.com>
2023-08-19 22:59:09 -0300

    Adição de atributo extensão

--- imagem.py
+++ imagem.py
@@ -2,11 +2,12 @@ from PIL import Image
 
 
 class Imagem:
-    def __init__(self, id, nome, caminho):
+    def __init__(self, id, nome, caminho,extensao):
         self.id = id
         self.nome = nome
         self.caminho = caminho
         self.imagem = None
+        self.extensao = extensao
         try:
             self.imagem = Image.open(self.caminho)
             print(f"Imagem: {self.nome} criada com sucesso")
@@ -18,3 +19,4 @@ class Imagem:
             self.imagem.show()
         except AttributeError:
             print("Imagem não encontrada ou não pode ser exibida.")
+            
