7acaf7c4c570ecfba9b8401399aad736471aa39c
Lucas Mendes <56414327+GeyldsonLucas@users.noreply.github.com>
2023-08-19 22:55:44 -0300

    Condições para adições de imagem, metodo buscar por id, listar com id

--- biblioteca.py
+++ biblioteca.py
@@ -3,6 +3,10 @@ class Biblioteca:
         self.lista_de_imagens = []
 
     def adicionar_imagem(self, imagem):
+        aux = self.buscar_imagem(imagem.nome)
+        if(aux != None and imagem.nome == aux.nome): 
+            return -1 
+       
         self.lista_de_imagens.append(imagem)
 
     def remover_imagem(self, imagem):
@@ -10,11 +14,16 @@ class Biblioteca:
 
     def listar_imagens(self):
         for imagem in self.lista_de_imagens:
-            print(imagem.nome)
+            print(str(imagem.id+1) + ' - ' +imagem.nome) 
 
     def buscar_imagem(self, nome):
         for imagem in self.lista_de_imagens:
             if imagem.nome == nome:
+                return imagem
+        return None
+    def buscar_imagem_id(self, id):
+        for imagem in self.lista_de_imagens:
+            if imagem.id == id:
                 return imagem
         return None
 
